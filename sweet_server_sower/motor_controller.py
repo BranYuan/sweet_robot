fe8d61ce76f82325d47a40c32f59fc5e1410a5b4
BranYuan <biaoyuan_shu@163.com>
2019-04-20 21:16:00 +0800

    完善播种机程序

--- sweet_server_sower/motor_controller.py
+++ sweet_server_sower/motor_controller.py
@@ -11,30 +11,37 @@ Created on 2019-3-7
 import SerialCmd
 import time
 
-MAX_SPEED = 2500    # 电机最大速度
-enables_motor = [0x01,0x10,0x46,0x57,0x00,0x01,0x02,0x00,0x01,0x4d,0xb3]    #电机使能
-disables_motor = [0x01,0x10,0x46,0x57,0x00,0x01,0x02,0x00,0x00,0x8c,0x73]   #电机取消使能
-speeds_cw =   [0x01,0x10,0x44,0x20,0x00,0x02,0x04,0x61,0xa8,0x00,0x00,0x6c,0xa8]    #电机正转速度,默认500
-speeds_ccw =  [0x01,0x10,0x44,0x20,0x00,0x02,0x04,0x9e,0x58,0xff,0xff,0x5d,0x3f]    #电机反转速度，默认-500
+MAX_SPEED = 3000    # 电机最大速度
+enables_motor = [0x01,0x06,0x00,0x46,0x7f,0xb2,0xc8,0x5a]    #电机使能
+disables_motor = [0x01,0x06,0x00,0xa9,0x00,0x00,0x59,0xea]   #电机停止，速度为0
+speeds_cw =   [0x01,0x06,0x00,0xa9,0x03,0x20,0x58,0xc2]    #电机正转速度,默认800
+speeds_ccw =  [0x01,0x06,0x00,0xa9,0xfc,0xe0,0x19,0x62]    #电机反转速度，默认-800
 
 
 ser_4G = SerialCmd.SerialCmd(port='/dev/ttyS4')     #4G信号通信端口
 ser = SerialCmd.SerialCmd()     #下位机通信端口
+
 ser_left = SerialCmd.SerialCmd(port='/dev/ttyS2')   #左电机通信端口
+ser_left.bauterate = 115200
+ser_left.stopbits = 2
+ser_left.set_port()
+
 ser_right = SerialCmd.SerialCmd(port='/dev/ttyS3')  #右电机通信端口
+ser_right.bauterate = 115200
+ser_right.stopbits = 2
+ser_right.set_port()
 
 
 # 遥控运行时，左右轮速度的计算函数
 def manul_speed_calculate(max_speed,speeds_cw,speeds_ccw):
-    if type(max_speed) is int and max_speed >= 0 and type(speeds_cw) is list and len(speeds_cw) == 13 and type(speeds_ccw) is list and len(speeds_ccw) == 13:
+    if type(max_speed) is int and max_speed >= 0 and type(speeds_cw) is list and len(speeds_cw) == 8 and type(speeds_ccw) is list and len(speeds_ccw) == 8:
         if max_speed > MAX_SPEED:
             max_speed = MAX_SPEED
-        max_speed *= 10
-        speeds_cw[7] = max_speed >> 8 & 0xff
-        speeds_cw[8] = max_speed & 0xff
+        speeds_cw[4] = max_speed >> 8 & 0xff
+        speeds_cw[5] = max_speed & 0xff
         max_speed = 0xffff - max_speed + 1
-        speeds_ccw[7] = max_speed >> 8 & 0xff
-        speeds_ccw[8] = max_speed & 0xff
+        speeds_ccw[4] = max_speed >> 8 & 0xff
+        speeds_ccw[5] = max_speed & 0xff
         crc_calculate(speeds_cw)
         crc_calculate(speeds_ccw)
 
@@ -56,7 +63,6 @@ def crc_calculate(data_list):
 
 # 自动运行
 def auto_run():
-    print("auto_run")
     cur_time = time.strftime("%y-%m-%d",time.localtime(time.time()))
     #log_file = open('/home/guardrobot/guardRobot/log_file/'+cur_time+'.txt','a')
     log_file =None  #open('/home/sweet/github_store/sweet_robot/4G_controller/log_file'+cur_time+'.txt','a')
@@ -66,7 +72,6 @@ def auto_run():
         ser_4G.open_port()
     #若端口打开，且有数据输入，则接收，并记录与logfile中
     if ser_4G.serial.isOpen():
-        print("waiting")
         while not ser_4G.serial.inWaiting():
             cmd_data = None
         else: # ser_4G.serial.inWaiting():
@@ -85,6 +90,7 @@ def auto_run():
         if end > head >-1:
             cmd_data = cmd_data[head:end+1]
         
+        print("speed\n")
         # 通过遥控改变最大运行速度
         #解析命令：SPEEDXXXX,XXXX为最大速度值
         if cmd_data.find("SPEED") == 1 and len(cmd_data) == 11:
@@ -94,7 +100,6 @@ def auto_run():
                 if not "0" <= i <= "9":
                     int_flag =False
             if int_flag:
-                print("speed\n")
                 max_speed = int(cmd_data)
                 # 计算速度指令
                 manul_speed_calculate(max_speed, speeds_cw, speeds_ccw)
@@ -110,6 +115,11 @@ def auto_run():
                         log_file.write("----speeds_ccw:")
                         for i in speeds_ccw:
                             log_file.write(str(hex(i)) + "  ")
+        
+        print("sleep")
+        print(cmd_data)
+        time.sleep(60)
+        print("wake_up")
 
 
         #下位机控制命令发送
@@ -134,6 +144,7 @@ def auto_run():
         
         #前进
         elif cmd_data == '*AHEAD#':
+            print("forward")
             if not ser_left.serial.isOpen():
                 ser_left.open_port()
             if ser_left.serial.isOpen():
